5b049a7cc72ed15c539b548ab176944c616f0795
Simone Cappella <48387175+SimoKappa@users.noreply.github.com>
2019-04-27 12:13:51 +0200

    Inserita navbar in Orario
    Devono adesso essere creati i diversi fragment per ogni giorno

--- Progetto/app/src/main/java/com/example/progetto/Clock.java
+++ Progetto/app/src/main/java/com/example/progetto/Clock.java
@@ -1,6 +1,8 @@
 package com.example.progetto;
 
 import android.graphics.Color;
+import android.renderscript.Sampler;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +18,9 @@ public class Clock extends AppCompatActivity {
         getWindow().getDecorView().setBackgroundColor(Color.parseColor("#cccccc"));
         Clock.ButtonHandler bh = new Clock.ButtonHandler();
         findViewById(R.id.back).setOnClickListener(bh);
+        BottomNavigationView nav = findViewById(R.id.navigation);
+        nav.setItemIconTintList(null);
+        nav.setItemIconSize(110);
     }
 
     private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.7F);
