2336616f2ac30305b692862b93f7e3b9f2d3fb05
Lucaman97 <s1080190@studenti.univpm.it>
2019-05-04 20:01:05 +0200

    Sistemate le impostazioni, creata l'activity per la modifica di username, email e password (da completare dopo il loginSession)

--- Progetto/app/src/main/java/com/example/progetto/Settings.java
+++ Progetto/app/src/main/java/com/example/progetto/Settings.java
@@ -1,20 +1,36 @@
 package com.example.progetto;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
         getWindow().getDecorView().setBackgroundColor(Color.parseColor("#cccccc"));
         ButtonHandler bh = new ButtonHandler();
         findViewById(R.id.back).setOnClickListener(bh);
+
+        TextView tv = findViewById(R.id.modifica_profilo);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchModificaProfilo(v);
+            }
+        });
+    }
+    public void launchModificaProfilo(View v)
+    {
+        Intent i = new Intent (this, ModificaProfilo.class);
+        startActivity(i);
     }
 
     private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.7F);
